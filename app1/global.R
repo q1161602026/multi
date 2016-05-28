11548555ae0bc3d2331a0a166d4c0f4f8584955f
q1161602026 <1161602026@qq.com>
2016-05-28 17:59:27 +0800

    Create global.R

---
+++ app1/global.R
@@ -0,0 +1,6 @@
+student=read.csv("student.csv",header=TRUE,stringsAsFactors=T,colClasses ="factor")
+student=student[-1]
+student$absences<-as.numeric(student$absences)
+student$G1<-as.numeric(student$G1)
+student$G2<-as.numeric(student$G2)
+student$G3<-as.numeric(student$G3)
